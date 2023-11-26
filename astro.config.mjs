3be6c1336d92a55277f2d81e6a284d8394edce32
Supass Ph. <112678332+SupaschaiPh@users.noreply.github.com>
2023-11-26 09:56:43 +0000

    add svelte&tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://supaschaiph.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://supaschaiph.github.io',
+  integrations: [mdx(), sitemap(), tailwind(), svelte()]
 });
